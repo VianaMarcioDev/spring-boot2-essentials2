cf9c25b66169cdc27674a5fc372701c8c7707f7e
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-12-12 19:38:42 -0300

    Add @Service in AnimeService class

--- src/main/java/br/com/zup/springboot2essentials2/service/AnimeService.java
+++ src/main/java/br/com/zup/springboot2essentials2/service/AnimeService.java
@@ -1,12 +1,14 @@
 package br.com.zup.springboot2essentials2.service;
 
 import br.com.zup.springboot2essentials2.domain.Anime;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class AnimeService {
 
     public List<Anime> listAll(){
-    return List.of(new Anime(1L,"Boku no Hero"), new Anime(2L,"Berserk"))
+    return List.of(new Anime(1L,"Boku no Hero"), new Anime(2L,"Berserk"));
     }
 }
