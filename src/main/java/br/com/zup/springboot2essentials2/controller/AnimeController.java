4d3453822c8ad40b598c63cbe68379eca4e2ff8b
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-12-12 19:33:05 -0300

    Add dependency injection AnimeService in AnimeController class

--- src/main/java/br/com/zup/springboot2essentials2/controller/AnimeController.java
+++ src/main/java/br/com/zup/springboot2essentials2/controller/AnimeController.java
@@ -1,6 +1,7 @@
 package br.com.zup.springboot2essentials2.controller;
 
 import br.com.zup.springboot2essentials2.domain.Anime;
+import br.com.zup.springboot2essentials2.service.AnimeService;
 import br.com.zup.springboot2essentials2.util.DateUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -19,11 +20,12 @@ public class AnimeController {
 
 
     private DateUtil dateUtil;
+    private final AnimeService animeService;
 
     @GetMapping
     public List<Anime> list(){
         log.info(dateUtil.formatlocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return ;
+        return animeService.listAll() ;
     }
 
 }
