d27a94d601959660553bf3a712c0136b79991b1d
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-12-12 19:15:17 -0300

    First commit. Paste packages controller, domain and util from spring2essentials project

---
+++ src/test/java/br/com/zup/springboot2essentials2/Springboot2Essentials2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.springboot2essentials2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2Essentials2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
